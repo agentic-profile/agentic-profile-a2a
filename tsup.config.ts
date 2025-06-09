1744438b2e832d86a647f78bc70c8a4e5a187137
Mike Prince <mike@mikeprince.com>
2025-06-08 20:47:00 -0700

    Fixed a2a-service dependency error

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,9 @@ import { defineConfig } from "tsup";
 export default defineConfig({
     entry: ["src/index.ts"],
     format: ["cjs", "esm"], // Build for commonJS and ESmodules
-    dts: true,
+    dts: {
+        resolve: true,
+    },
     splitting: false,
     sourcemap: true,
     clean: true,
